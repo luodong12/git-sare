7bbf12d062c0d9c2150155efa1079db545dccede
LuoDong <1908326656@qq.com>
2023-02-25 14:13:07 +0800

    third-git2

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -4,7 +4,7 @@ package com.itguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git1");
-
+        System.out.println("hello git2");
     }
 
 }
