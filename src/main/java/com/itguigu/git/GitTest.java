3c59893dd350e3dbbccc4b3269de089c8009c411
LuoDong <1908326656@qq.com>
2023-07-27 15:58:39 +0800

    git-01

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -3,14 +3,8 @@ package com.itguigu.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git1");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hot-fix git4");
-        System.out.println("master git5");
-        System.out.println("hot-fix git5");
-        System.out.println("push-fix git5");
-        System.out.println("pull-fix git5");
+        System.out.println("hello,你好");
+
     }
 
 }
