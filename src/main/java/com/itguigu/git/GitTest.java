92991f15cbc6b3f82379a8da0b199df6fe00975a
LuoDong <1908326656@qq.com>
2023-07-27 15:59:51 +0800

    git-02

--- src/main/java/com/itguigu/git/GitTest.java
+++ src/main/java/com/itguigu/git/GitTest.java
@@ -4,6 +4,7 @@ package com.itguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,你好");
+        System.out.println("hello,git-02");
 
     }
 
